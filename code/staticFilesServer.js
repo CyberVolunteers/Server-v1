7d4bc9e4c9f185639d84ece93532606b699c76ee
Mikhail Nekrasov <misha.nekrasov@gmail.com>
2020-06-26 17:03:21 +0100

    Added the static page server

--- code/staticFilesServer.js
+++ code/staticFilesServer.js
@@ -1,8 +1,8 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = 1234;
 
-app.get('/', (req, res) => res.send('Hello World!'));
-app.get('/helloworld', (req, res) => res.send('Hello World! HEYY'));
+app.use('/', express.static(path.join(__dirname, 'public')));
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
