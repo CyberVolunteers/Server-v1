b4f85ab9272f2e144f7c0c03de018fa980718d4f
Mikhail Nekrasov <misha.nekrasov@gmail.com>
2020-07-04 19:24:31 +0100

    updated the static file server for hbs and renamed some files

--- code/staticFilesServer.js
+++ code/staticFilesServer.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const path = require('path');
+const exphbs  = require('express-handlebars');
 const app = express();
 const port = 1234;
 
+app.engine('hbs', exphbs());
+app.set('view engine', 'hbs');
+
+// serve the pages
+app.get("*", function(req, res, next){
+    if(req.url.includes(".")) return next();
+
+    res.render(__dirname + "/public" + req.url + "index.hbs", {layout: false});
+})
 app.use('/', express.static(path.join(__dirname, 'public')));
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
