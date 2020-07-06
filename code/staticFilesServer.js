e03f833c7ea94d0345aa75c53d40a77f75afd7d6
Mikhail Nekrasov <misha.nekrasov@gmail.com>
2020-07-06 20:03:45 +0100

    fixed static files server

--- code/staticFilesServer.js
+++ code/staticFilesServer.js
@@ -6,13 +6,17 @@ const port = 1234;
 
 app.engine('hbs', exphbs());
 app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, '/public'));
 
 // serve the pages
 app.get("*", function(req, res, next){
     if(req.url.includes(".")) return next();
 
-    res.render(__dirname + "/public" + req.url + "index.hbs", {layout: false});
+    res.render(__dirname + "/public" + req.url + "/" + "index.hbs", {layout: false});
 })
+
+
+
 app.use('/', express.static(path.join(__dirname, 'public')));
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
