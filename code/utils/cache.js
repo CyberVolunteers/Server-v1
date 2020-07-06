582c1e396b147af1a3de6054c48baaa247acd8ec
Mikhail Nekrasov <misha.nekrasov@gmail.com>
2020-07-06 19:53:15 +0100

    added a cache dependancy

---
+++ code/utils/cache.js
@@ -0,0 +1,8 @@
+const nodeCache = require("node-cache");
+
+class Cache {
+    constructor(ttlSeconds) {
+        this.cache = new nodeCache.NodeCache({ stdTTL: ttlSeconds, checkperiod: ttlSeconds * 0.2, useClones: false });
+    }
+}
+  
