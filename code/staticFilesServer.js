2a642ee04c74ad00cbbcf0045e188fe15b8e7db6
Mikhail Nekrasov <misha.nekrasov@gmail.com>
2020-07-09 19:40:45 +0100

    sorted files out

--- code/staticFilesServer.js
+++ code/staticFilesServer.js
@@ -1,18 +1,19 @@
-const express = require('express');
-const path = require('path');
-const exphbs  = require('express-handlebars');
+const express = require("express");
+const path = require("path");
+const exphbs  = require("express-handlebars");
 const app = express();
 const port = 1234;
 
 app.engine('hbs', exphbs());
 app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, '/public'));
 
 // serve the pages
 app.get("*", function(req, res, next){
-    if(req.url.includes(".")) return next();
+	if(req.url.includes(".")) return next();
 
-    res.render(__dirname + "/public" + req.url + "index.hbs", {layout: false});
-})
-app.use('/', express.static(path.join(__dirname, 'public')));
+	res.render(__dirname + "/public" + req.url + "index.hbs", {layout: false});
+});
+app.use("/", express.static(path.join(__dirname, "public")));
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
