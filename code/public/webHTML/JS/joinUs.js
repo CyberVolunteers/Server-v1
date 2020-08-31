282aa3bd3d855e0a8eb823c51a0bbbe0996c645f
Mikhail Nekrasov <misha.nekrasov@gmail.com>
2020-08-31 11:46:38 +0100

    merged from client

--- code/public/webHTML/JS/joinUs.js
+++
@@ -1,8 +0,0 @@
-$(function(){
-	$(".volunteerJoinButton").click(function(){
-        window.location.href = `${window.location.protocol}//${window.location.host}/volunteerSignUp`;
-    })
-    $(".charityJoinButton").click(function(){
-        window.location.href = `${window.location.protocol}//${window.location.host}/charitySignUp`;
-    })
-});
