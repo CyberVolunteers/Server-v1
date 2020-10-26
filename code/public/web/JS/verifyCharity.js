8ceb8579f0fb9236bce831d1894649bec1f893ed
Mikhail Nekrasov <misha.nekrasov@gmail.com>
2020-10-26 21:30:17 +0000

    added new headers

--- code/public/web/JS/verifyCharity.js
+++ code/public/web/JS/verifyCharity.js
@@ -1,10 +1,8 @@
 $(function(){
     $("#submit").click(function(){
-        console.log($("#verifyEmail").is(':checked'));
         $.post("/verifyCharity",
         {
-            id: $("#verifyCharity").val(),
-            verifyEmail: $("#verifyEmail").is(':checked')
+            id: $("#verifyCharity").val()
         })
         .done(function(data, textStatus){
             console.log(data.message);
